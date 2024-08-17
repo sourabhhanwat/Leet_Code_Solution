5b217cb44472e28599de3adeba714fd09e3a3f97
sourabh hanwat <hanwat007@gmail.com>
2024-08-17 14:50:43 +0530

    Time: 43 ms (92.54%), Space: 16.5 MB (82.21%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.py
+++ 0009-palindrome-number/0009-palindrome-number.py
@@ -3,8 +3,7 @@ class Solution:
         real, rev_x = x, 0
 
         while x > 0:
-            digit = x % 10
-            rev_x = rev_x * 10 + digit
+            rev_x = rev_x * 10 + (x % 10)
             x = x//10
 
         return True if rev_x == real else False
